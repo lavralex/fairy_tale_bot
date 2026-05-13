0ea9d774a79da3b1325115f50ec7f3be4385dd08
lavralex <lavralex2015@yandex.ru>
2026-05-13 15:57:44 +0300

    feat: add user model and CreateUser method

---
+++ internal/models/user.go
@@ -0,0 +1,24 @@
+package models
+
+import (
+	"time"
+)
+
+type UserRole string
+
+const (
+	RoleUser       UserRole = "user"
+	RoleAdmin      UserRole = "admin"
+	RoleSuperAdmin UserRole = "superadmin"
+)
+
+type User struct {
+	ID         int64
+	TelegramID int64
+	Username   string
+	Phone      string
+	Email      string
+	CreatedAt  time.Time
+	Role       UserRole
+	IsBanned   bool
+}
