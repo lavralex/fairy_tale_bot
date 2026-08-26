d8db08a80a02f815a7d4d1e92ccab47ebd1ebc58
lavralex <lavralex2015@yandex.ru>
2026-08-26 21:02:50 +0300

    feat(storage): add DeleteTemplate and UpdateTemplate

--- internal/storage/templates.go
+++ internal/storage/templates.go
@@ -197,3 +197,132 @@ func (s *Storage) GetTemplate(ctx context.Context, id int64) (models.Template, e
 	}
 	return template, nil
 }
+
+func (s *Storage) GetTemplates(ctx context.Context, limit int, offset int) ([]models.Template, error) {
+	rows, err := s.db.Query(
+		ctx,
+		"SELECT id, name, created_at FROM templates "+
+			"LIMIT $1 OFFSET $2", limit, offset,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("GetTemplates query: %w", err)
+	}
+	defer rows.Close()
+	var templates = make([]models.Template, 0)
+	for rows.Next() {
+		var template models.Template
+		err := rows.Scan(
+			&template.ID,
+			&template.Name,
+			&template.CreatedAt,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("GetTemplates Scan: %w", err)
+		}
+		templates = append(templates, template)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, fmt.Errorf("GetTemplates rows: %w", err)
+	}
+	return templates, nil
+}
+
+func (s *Storage) DeleteTemplate(ctx context.Context, id int64) error {
+	ct, err := s.db.Exec(ctx, "DELETE FROM templates WHERE id = $1", id)
+	if err != nil {
+		return fmt.Errorf("DeleteTemplate: %w", err)
+	}
+	if ct.RowsAffected() == 0 {
+		return ErrTemplateNotFound
+	}
+	return nil
+}
+
+func (s *Storage) UpdateTemplate(ctx context.Context, template models.Template) (models.Template, error) {
+	if len(template.Fields) < 1 {
+		return models.Template{}, ErrIncorrectNumberOfFields
+	}
+	tx, err := s.db.Begin(ctx)
+	if err != nil {
+		return models.Template{}, fmt.Errorf("UpdateTemplate begin: %w", err)
+	}
+	defer tx.Rollback(ctx)
+	row := tx.QueryRow(
+		ctx,
+		"UPDATE templates SET name = $1 WHERE id = $2 "+
+			"RETURNING id, name, created_at",
+		template.Name, template.ID,
+	)
+	var returnedTemplate models.Template
+	err = row.Scan(
+		&returnedTemplate.ID,
+		&returnedTemplate.Name,
+		&returnedTemplate.CreatedAt,
+	)
+	if errors.Is(err, pgx.ErrNoRows) {
+		return models.Template{}, ErrTemplateNotFound
+	}
+	if err != nil {
+		return models.Template{}, fmt.Errorf("UpdateTemplate template scan: %w", err)
+	}
+	_, err = tx.Exec(
+		ctx,
+		"DELETE FROM template_fields WHERE template_id = $1", template.ID,
+	)
+	if err != nil {
+		return models.Template{}, fmt.Errorf("UpdateTemplate fields delete: %w", err)
+	}
+	for order, field := range template.Fields {
+		_, err := tx.Exec(
+			ctx,
+			"INSERT INTO template_fields (template_id, field_id, sort_order) "+
+				"VALUES ($1, $2, $3);",
+			returnedTemplate.ID, field.Field.ID, order,
+		)
+		if err != nil {
+			return models.Template{}, fmt.Errorf("UpdateTemplate: %w", err)
+		}
+	}
+	rows, err := tx.Query(
+		ctx,
+		"SELECT f.id, f.name, f.is_active, f.created_at, tf.sort_order "+
+			"FROM template_fields AS tf "+
+			"JOIN fields AS f ON f.id = tf.field_id "+
+			"WHERE tf.template_id = $1 "+
+			"ORDER BY tf.sort_order",
+		returnedTemplate.ID,
+	)
+	if err != nil {
+		return models.Template{}, fmt.Errorf("UpdateTemplate fields query: %w", err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var field models.Field
+		var order int
+		err = rows.Scan(
+			&field.ID,
+			&field.Name,
+			&field.IsActive,
+			&field.CreatedAt,
+			&order,
+		)
+		if err != nil {
+			return models.Template{}, fmt.Errorf("UpdateTemplate field scan: %w", err)
+		}
+		returnedTemplate.Fields = append(
+			returnedTemplate.Fields,
+			models.TemplateField{
+				Field:     field,
+				SortOrder: order,
+			},
+		)
+	}
+	if err = rows.Err(); err != nil {
+		return models.Template{}, fmt.Errorf("UpdateTemplate fields rows: %w", err)
+	}
+	err = tx.Commit(ctx)
+	if err != nil {
+		return models.Template{}, fmt.Errorf("UpdateTemplate commit: %w", err)
+	}
+	return returnedTemplate, nil
+}
